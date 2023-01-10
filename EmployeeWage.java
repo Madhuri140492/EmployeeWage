894b3b8ed60c9413f7588ad8d035e539813db02d
Madhuri140492 <madhurideshmukh1404@gmail.com>
2023-01-10 20:55:04 +0530

    [Added : Employee Attendance Check]

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,3 +8,16 @@ public class EmployeeWage
             System.out.println("Welcome to Employee Wage Computation Program");
         }
     }
+
+    package com.bridgelabz.employeewage;
+/*Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check*/
+public class EmpCheck {
+    public static void main(String[] args) {
+        int IS_PRESENT = 1;
+        int employeeCheck = (int)Math.floor(Math.random() * 10) % 2;
+        if ( employeeCheck == 1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+    }
+}
